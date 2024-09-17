57ee76125e3a7de019356a8733c84ae4e63ef050
Italo Andrade <ifa2@cin.ufpe.br>
2024-09-16 21:32:55 -0300

    use next image and add style in components products

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,6 +1,8 @@
 import { productType } from "@/types/productType";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "./ui/card";
 import { Button } from "./ui/button";
+import ProductImage from "./productImage";
+import Image from "next/image";
 
 type ProductProps = {
     product: productType,
@@ -8,10 +10,13 @@ type ProductProps = {
 
 export default function Product( { product } : ProductProps){
     return (
-        <Card className="flex flex-col shadow-lg h-96 w-60 bg-slate-800 p-5 text-gray-300">
-            <CardTitle className="relative max-h-72 flex-1">IMG</CardTitle>
-            <CardContent className="flex justify-between font-bold my-3">{product.title}</CardContent>
-            <Button className="bg-teal-600 text-white p-5">Adicionar ao carrinho</Button>
+        <Card className="flex flex-col shadow-lg h-96 w-60 p-4 bg-slate-800  text-gray-300">
+            <CardTitle className="relative max-h-72 flex-1"><ProductImage product={product} fill></ProductImage></CardTitle>
+            <CardContent className="flex justify-between p-0 font-bold my-3">
+                    <p className="text-sm w-36 truncate">{product.title}</p>
+                    <p className="text-md text-teal-300">{product.price}</p>
+            </CardContent>
+            <Button className="bg-teal-600 text-white p-5 mt-auto">Adicionar ao carrinho</Button>
         </Card>
     )
 }
